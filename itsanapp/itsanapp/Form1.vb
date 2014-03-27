849ed1318fc6b82fa20c6b951fc873c7d96adc31
Sai-Akuto <jack.kitchener@gmail.com>
2014-03-27 11:46:13 +0800

    Added Sai_Akuto

--- itsanapp/itsanapp/Form1.vb
+++ itsanapp/itsanapp/Form1.vb
@@ -3,9 +3,14 @@
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        names.Items.Add("Sai Akuto")
+        For i = 0 To 3
+            names.Items.Add(" ")
+        Next i
         names.Items.Add("immabmo")
+
     End Sub
+
 End Class
 
 
